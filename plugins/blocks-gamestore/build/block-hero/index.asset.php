ffc6b98ed7e36db0f96c99bb7efef1845acb3317
PerfZero1 <gundenis1@gmail.com>
2025-09-20 13:47:51 +0300

    plugin

---
+++ plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5f80524a643b7f746911');
---
+++ plugins/core-gemastore/core-gemastore.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Core Gemastore
+ * Description: Core functionality for Gemastore - jewelry and gemstone e-commerce platform
+ * Version: 1.0.0
+ * Author: Devdenis
+ * Author URI: https://devdenis.ru
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: core-gemastore
+ * Domain Path: /languages
+ */
+
+ define('GAMESTORE_PLUGIN_URL' ,  plugin_dir_url(__FILE__));
+ define('GAMESTORE_PLUGIN_PARH' ,  plugin_dir_path(__FILE__));
+ 
